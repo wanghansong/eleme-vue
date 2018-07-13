23040c4631741dbc529249545c068d2622ffe73a
wanghansong <wanghansong@wanghansongdeMacBook-Pro.local>
2018-07-13 16:57:48 +0800

    router

--- src/router/index.js
+++ src/router/index.js
@@ -4,26 +4,34 @@ import Router from 'vue-router';
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
     mode: 'history',
     routes: [
         {
             path: '/',
             alias: '/goods',
             name: 'Goods',
-            meta: '商品',
+            meta: {
+                title: '商品'
+            },
             component: () =>
               import ( /* webpackChunkName:"goods/index" */ '@/pages/goods/index')
         },
         {
             path: '/ratings',
             name: 'Ratings',
+            meta: {
+                title: '评价'
+            },
             component: () =>
               import ( /* webpackChunkName:"ratings/index" */ '@/pages/ratings/index')
         },
         {
             path: '/seller',
             name: 'Seller',
+            meta: {
+                title: '商家'
+            },
             component: () =>
               import ( /* webpackChunkName:"seller/index" */ '@/pages/seller/index')
         },
@@ -34,3 +42,13 @@ export default new Router({
         }
     ]
 });
+router.beforeEach((to, from, next) => {
+    window.document.title = to.meta.title;
+    next();
+});
+router.afterEach( () => {
+    window.scrollTo(0, 0);
+});
+
+
+export default router;
